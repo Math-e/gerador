71aedac3798e90f658df4b4a9ecf41f66b8762a1
Math-e <32374265+Math-e@users.noreply.github.com>
2021-11-02 10:42:15 -0300

    initial commit

---
+++ android/app/src/main/kotlin/com/example/gerador/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gerador
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[600]),
+        home: RandomWords());
+  }
+}
